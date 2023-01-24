5da8f8bc423a324890d85f7bc030e2c7a2e4ccb6
Kae Sluder <kaesluder@fastmail.com>
2023-01-23 20:30:36 -0500

    parsing feed items from raw xml

--- src/netLibs/feedDataClasses.ts
+++ src/netLibs/feedDataClasses.ts
@@ -14,4 +14,21 @@ interface Feed {
   generator?: string;
 }
 
-export { Feed };
+interface Item {
+  // Drama Llama internal fields
+  id?: number;
+  feedID?: number;
+
+  // RSS fields
+
+  title?: string; //	The title of the item.	Venice Film Festival Tries to Quit Sinking
+  link?: string; // 	The URL of the item.	http://nytimes.com/2004/12/07FEST.html
+  description?: string; //	The item synopsis.	<description>Some of the most heated chatter at the Venice Film Festival this week was about the way that the arrival of the stars at the Palazzo del Cinema was being staged.</description>
+  author?: string; //	Email address of the author of the item. More.
+  category?: string; //	Includes the item in one or more categories. More.
+  guid?: string; //	A string that uniquely identifies the item. More.
+  pubDate?: number; //	Indicates when the item was published. More.
+  source?: string; //
+}
+
+export { Feed, Item };
--- src/netLibs/rssParser.test.ts
+++ src/netLibs/rssParser.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
-import { convertDate, parseRSS } from "./rssParser";
+import { convertDate, itemBuilder, parseRSS, parseRSSItems } from "./rssParser";
 import { readFileSync } from "fs";
-import { Feed } from "./feedDataClasses";
+import { Feed, Item } from "./feedDataClasses";
 import * as R from "ramda";
 
 const EXAMPLE_RSS = readFileSync("test-data/rss2sample.xml").toString();
@@ -28,5 +28,39 @@ describe("Testing parsing of rss from text string and creation of feed and item 
     const parsed: Feed = parseRSS(EXAMPLE_RSS);
     expect(R.prop("title")(parsed)).toBe("Liftoff News");
     expect(R.prop("pubDate")(parsed)).toBe(1055217600000);
+  });
+
+  test("construct new Item", () => {
+    const testItem = {
+      title: "title",
+      link: "http://example.com",
+      description: "A test feed",
+      author: "a test author",
+      pubDate: "Mon, 04 Nov 1985 19:53:20 EST",
+    };
+    const parsed: Item = itemBuilder(testItem);
+    expect(parsed.title).toBe("title");
+  });
+
+  test("itemBuilder constructs random uuid for empty guid", () => {
+    const testItem = {
+      title: "title",
+      link: "http://example.com",
+      description: "A test feed",
+      author: "a test author",
+      pubDate: "Mon, 04 Nov 1985 19:53:20 EST",
+    };
+    const parsed: Item = itemBuilder(testItem);
+    console.log(parsed.guid);
+    expect(parsed.guid).toMatch(
+      /^[a-f0-9]{8,}-[a-f0-9]{4,}-[a-f0-9]{4,}-[a-f0-9]{4,}-[a-f0-9]{12,}/
+    );
+  });
+
+  test("extract list of items from xml string", () => {
+    const items = parseRSSItems(EXAMPLE_RSS);
+    expect(items[0].title).toBe("Star City");
+    expect(items.length).toBe(4);
+    console.log(items);
   });
 });

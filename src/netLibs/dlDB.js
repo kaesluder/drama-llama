9eee63627cf28d0372e43d425af2974830eda9fb
Kae Sluder <kaesluder@fastmail.com>
2023-01-26 15:26:41 -0500

    refactor to javascript

--- src/netLibs/dlDB.js
+++ src/netLibs/dlDB.js
@@ -6,21 +6,21 @@ const DB_DIR = "/tmp/dldbtest";
 
 const database = new PouchDB(DB_DIR);
 
-const handleError = function (err: object) {
+const handleError = function (err) {
   console.log(R.pickAll(["status", "name", "conflict", "id", "docId"])(err));
 };
 
-const handleBulkError = function (errList: object[]) {
+const handleBulkError = function (errList) {
   errList.map(handleError);
 };
 
-const saveFeed = function (feed: Feed) {
+const saveFeed = function (feed) {
   const id = genFeedID(feed);
   return database.put(R.assoc("_id", id, feed)).catch(handleError);
 };
 
-const saveItems = function (itemList: Item[]) {
-  const mapFun = (i: Item) => R.assoc("_id", genItemID(i))(i);
+const saveItems = function (itemList) {
+  const mapFun = (i) => R.assoc("_id", genItemID(i))(i);
   return database
     .bulkDocs(itemList.map(mapFun))
     .then(function (err) {
@@ -36,9 +36,9 @@ const getAllDocs = function () {
   return database.allDocs({ include_docs: true });
 };
 
-const mergeWithPartial = function (full: object, partial: object) {
+const mergeWithPartial = function (full, partial) {
   // make sure that partial doesn't have _rev and merge
-  const newRecord: object = { ...full, ...R.dissoc("_rev", partial) };
+  return { ...full, ...R.dissoc("_rev", partial) };
 };
 
 export { saveFeed, getAllDocs, saveItems, database };

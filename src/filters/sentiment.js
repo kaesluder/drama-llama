222b8dfc00441c3387a8253f6ea65d24bdff2a2b
Kae Sluder <kaesluder@fastmail.com>
2023-01-27 13:06:25 -0500

    comment edits

--- src/filters/sentiment.js
+++ src/filters/sentiment.js
@@ -5,6 +5,13 @@ const stemmer = function (text) {
   return PorterStemmer.tokenizeAndStem(text);
 };
 
+/**
+ * sentiment analyzes text and returns a score between -1 and 1
+ *
+ *
+ * @param {string} text
+ * @returns {int} score (between -1 and 1)
+ */
 const sentiment = function (text) {
   const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
   const tokenizer = new WordTokenizer();
